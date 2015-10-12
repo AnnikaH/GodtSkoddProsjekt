6699a75d04c8b2fa6910600759c6a6afd7aed034
AnnikaH <annika_limfe@hotmail.com>
2015-10-12 13:54:31 +0200

    LoginUser created, Login-method in HomeController

--- GodtSkoddProsjekt/Controllers/HomeController.cs
+++ GodtSkoddProsjekt/Controllers/HomeController.cs
@@ -12,11 +12,39 @@ namespace GodtSkoddProsjekt.Controllers
         public ActionResult Index()
         {
             // tester:
-            var dbGodtSkodd = new DBGodtSkodd();
-            dbGodtSkodd.test();
+            // var dbGodtSkodd = new DBGodtSkodd();
+            // dbGodtSkodd.test();
             // tester til hit
 
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogIn(LoginUser user)
+        {
+            // checking log in
+
+            var dbGodtSkodd = new DBGodtSkodd();
+
+            if (dbGodtSkodd.UserInDb(user))
+            {
+                // yes username and password is OK
+                Session["LoggedIn"] = true;
+                ViewBag.Innlogget = true;
+                // return RedirectToAction("Index");
+            }
+            else
+            {
+                // no
+                Session["LoggedIn"] = false;
+                ViewBag.Innlogget = false;
+                // return RedirectToAction("Index");
+            }
+
+            // Can use ViewBag in the view!
+
+            return RedirectToAction("Index");
         }
 
         // GET: Home/Details/5
