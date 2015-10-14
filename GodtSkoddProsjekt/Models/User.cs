0cd1028acf8f7a0216c124e668e11b256e17511e
hannerygge <s198613@stud.hioa.no>
2015-10-14 18:23:07 +0200

    fiksa regex

--- GodtSkoddProsjekt/Models/User.cs
+++ GodtSkoddProsjekt/Models/User.cs
@@ -14,36 +14,39 @@ namespace GodtSkoddProsjekt.Models
 // FÅR FEILMELDING PÅ REGEX'ENE NÅR KJØRER Create.cshtml: ÆØÅ?
         [Display(Name = "Fornavn")]
         [Required(ErrorMessage = "Fornavn må oppgis")]
-        //[RegularExpression(@"[A-ZÆØÅa-zæøå]{30}", ErrorMessage = "Fornavn kan bare inneholde bokstaver fra A-Å, og være 30 bokstaver langt.")]
+        [RegularExpression(@"[A-ZÆØÅa-zæøå]{1,30}", ErrorMessage = "Fornavn kan bare inneholde bokstaver fra A-Å")]
         public String firstName { get; set; }
 
         [Display(Name = "Etternavn")]
         [Required(ErrorMessage = "Etternavn må oppgis")]
-        //[RegularExpression(@"[A-ZÆØÅa-zæøå]{30}", ErrorMessage = "Etternavn kan bare inneholde bokstaver fra A-Å, og være 30 bokstaver langt.")]
+        [RegularExpression(@"[A-ZÆØÅa-zæøå]{1,30}", ErrorMessage = "Etternavn kan bare inneholde bokstaver fra A-Å.")]
         public String lastName { get; set; }
 
         [Display(Name = "Adresse")]
         [Required(ErrorMessage = "Adressen må oppgis")]
-        //[RegularExpression(@"[A-ZÆØÅa-zæøå0-9]{30}", ErrorMessage = "Adresse kan bare inneholde bokstaver fra A-Å og tall, og være 30 bokstaver langt.")]
+        [RegularExpression(@"[A-ZÆØÅa-zæøå0-9\s]{1,30}", ErrorMessage = "Adresse kan bare inneholde bokstaver fra A-Å og tall.")]
         public String address { get; set; }
 
         [Display(Name = "E-post")]
         [Required(ErrorMessage = "E-post må oppgis")]
-        //[RegularExpression(@"[A-ZÆØÅa-zæøå0-9_-@.]{30}", ErrorMessage = "E-post")]
+        [RegularExpression(@"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*"+ "@"+ @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$", ErrorMessage = "E-post")]
         public String email { get; set; }
+
+//@"[A-ZÆØÅa-zæøå0-9_-@.]{1,30}"
 
         [Display(Name = "Telefonnr")]
         [Required(ErrorMessage = "Telefonnr må oppgis")]
-        //[RegularExpression(@"[0-9]{8}", ErrorMessage = "Telefonnummer kan bare inneholde 8 tall")]
+        [RegularExpression(@"[0-9]{8}", ErrorMessage = "Telefonnummer kan bare inneholde 8 tall")]
         public String phoneNumber { get; set; }
 
         [Display(Name = "Postnr")]
         [Required(ErrorMessage = "Postnr må oppgis")]
-        //[RegularExpression(@"[0-9]{4}", ErrorMessage = "Postnr må være 4 siffer")]
+        [RegularExpression(@"[0-9]{4}", ErrorMessage = "Postnr må være 4 siffer")]
         public String postalCode { get; set; }
 
         [Display(Name = "Poststed")]
         [Required(ErrorMessage = "Poststed må oppgis")]
+        [RegularExpression(@"[A-ZÆØÅa-zæøå]{1,30}", ErrorMessage = "Poststed kan bare inneholde bokstaver fra A-Å.")]
         public String city { get; set; }
 
         [Display(Name = "Brukernavn")]
