9e282717bc8b8692ceb5190f2e69683d60acf5c1
AnnikaH <annika_limfe@hotmail.com>
2015-10-26 17:55:41 +0100

    Created project start for task 2 (BLL, DAL, GodtSkoddProsjekt and Model)

---
+++ BLL/BusinessLogic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BusinessLogic
+    {
+        // var kundeDAL = new DAL();
+        /* Fra Tor sitt eksempel (Lagdeling):
+        public List<Kunde> hentAlle()
+        {
+            var KundeDAL = new KundeDAL();
+            List<Kunde> alleKunder = KundeDAL.hentAlle();
+            return alleKunder;
+        }
+        public bool settInn(Kunde innKunde)
+        {
+            var KundeDAL = new KundeDAL();
+            return KundeDAL.settInn(innKunde);
+        }
+        public bool endreKunde(int id, Kunde innKunde)
+        {
+            var KundeDAL = new KundeDAL();
+            return KundeDAL.endreKunde(id, innKunde);
+        }
+        public bool slettKunde(int slettId)
+        {
+            var KundeDAL = new KundeDAL();
+            return KundeDAL.slettKunde(slettId);
+        }
+        public Kunde hentEnKunde(int id)
+        {
+            var KundeDAL = new KundeDAL();
+            return KundeDAL.hentEnKunde(id);
+        }
+        */
+    }
+}
---
+++ GodtSkoddProsjekt/Controllers/ADMINMainController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GodtSkoddProsjekt.Controllers
+{
+    public class ADMINMainController : Controller
+    {
+        //---------------------- TOR SIN KODE FRA TIMEN (Lagdeling): ------------------------//
+        /*
+        public ActionResult Liste()
+        {
+            var kundeDb = new KundeLogikk();
+            List<Kunde> alleKunder = kundeDb.hentAlle();
+            return View(alleKunder);
+        }
+
+        public ActionResult Detaljer(int id)
+        {
+            var kundeDb = new KundeLogikk();
+            Kunde enKunde = kundeDb.hentEnKunde(id);
+            return View(enKunde);
+        }
+
+        public ActionResult Registrer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrer(Kunde innKunde)
+        {
+            if (ModelState.IsValid)
+            {
+                var kundeDb = new KundeLogikk();
+                bool insertOK = kundeDb.settInn(innKunde);
+                if (insertOK)
+                {
+                    return RedirectToAction("Liste");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Endre(int id)
+        {
+            var kundeDb = new KundeLogikk();
+            Kunde enKunde = kundeDb.hentEnKunde(id);
+            return View(enKunde);
+        }
+
+        [HttpPost]
+        public ActionResult Endre(int id, Kunde endreKunde)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var kundeDb = new KundeLogikk();
+                bool endringOK = kundeDb.endreKunde(id, endreKunde);
+                if (endringOK)
+                {
+                    return RedirectToAction("Liste");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Slett(int id)
+        {
+            var kundeDb = new KundeLogikk();
+            Kunde enKunde = kundeDb.hentEnKunde(id);
+            return View(enKunde);
+        }
+
+        [HttpPost]
+        public ActionResult Slett(int id, Kunde slettKunde)
+        {
+            var kundeDb = new KundeLogikk();
+            bool slettOK = kundeDb.slettKunde(id);
+            if(slettOK)
+            {
+                return RedirectToAction("Liste");
+            }
+            return View();
+        }
+        */
+
+        //---------------------- AUTOGENERERT KODE: ---------------------------------//
+
+        // GET: ADMINMain
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ADMINMain/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ADMINMain/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ADMINMain/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ADMINMain/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ADMINMain/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ADMINMain/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ADMINMain/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
