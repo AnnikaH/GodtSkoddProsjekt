ace20b3bebad524986146c58b721297366a134f8
Proetic <datafriken_1@hotmail.com>
2015-11-07 14:45:29 +0100

    fixed stuff

--- DAL/DBGodtSkodd.cs
+++ DAL/DBGodtSkodd.cs
@@ -872,6 +872,7 @@ namespace DAL
                     Orders foundOrder = db.Orders.Find(id);
                     returnOrder.id = foundOrder.ID;
                     returnOrder.userID = foundOrder.UserID;
+                    returnOrder.date = foundOrder.Date;
                     foreach (var item in foundOrder.Orderlines)
                     {
                         Orderline newOrderLine = new Orderline();
